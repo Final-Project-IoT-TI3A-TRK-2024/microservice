994c1849dbcfeff71237a527f3e5f487267b1416
Naufal Reky Ardhana <rekyardhana029@gmail.com>
2024-11-29 09:37:59 +0700

    chore(env): default url is null

--- mqtt-client-service.py
+++ mqtt-client-service.py
@@ -9,7 +9,7 @@ load_dotenv()
 os.environ['TZ'] = 'Asia/Jakarta'
 
 try:
-    mongo = pymongo.MongoClient(os.getenv('MONGO_URI') or 'mongodb://localhost:27017/')
+    mongo = pymongo.MongoClient(os.getenv('MONGO_URI'))
     db = mongo['chart']
     collection = db['sensor']
 except Exception as e:
